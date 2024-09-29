8375e6594ec7ed9c1fe4d93e98a8fb6c2681b1b8
Messmer <prokopovaleksandr544@gmail.com>
2024-09-29 12:33:47 +0700

    modified model and migration

--- database/migrations/2024_09_29_051716_create_users_table.php
+++ database/migrations/2024_09_29_051716_create_users_table.php
@@ -13,7 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('username')->unique();
+            $table->string('name');
+            $table->string('surname');
+            $table->string('patronymic');
+            $table->string('phoneNumber')->unique();
+            $table->string('email')->unique();
             $table->timestamps();
+
+            $table->softDeletes();
         });
     }
 
