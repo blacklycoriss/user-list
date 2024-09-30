05774d46f0d350ea6a37c40fd6244c5094754985
Messmer <prokopovaleksandr544@gmail.com>
2024-09-30 18:05:12 +0700

    add create new user link

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.layout')
 @section('content')
     <h3>Все пользователи сайта</h3>
+    <span><a href="/users/create">Создать нового пользователя</a></span>
     <p>Имя | Электронная почта</p>
     @foreach($users as $user)
         <li>{{ $user->name . ' | ' . $user->email}}</li>
