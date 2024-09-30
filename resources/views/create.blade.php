c04da05fe7a599cc4db79318a43c5c52906f06ec
Messmer <prokopovaleksandr544@gmail.com>
2024-09-30 18:25:23 +0700

    add create form

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.layout')
 @section('content')
 
+    <h3>Создать нового пользователя на сайте</h3>
+    <br>
+    {{ html()->form('POST')->open() }}
+        Логин: {{ html()->input('text', 'username') }} <br>
+        Имя: {{ html()->input('text', 'name') }} <br>
+        Фамилия: {{ html()->input('text', 'surname') }} <br>
+        Отчество/Матчество: {{ html()->input('text', 'patronymic') }} <br>
+        Телефон: {{ html()->input('text', 'phoneNumber') }} <br>
+        Электронная почта: {{ html()->input('email', 'email') }} <br>
+        <br>
+        {{ html()->submit('Создать') }}
+    {{ html()->form()->close() }}
 @endsection
