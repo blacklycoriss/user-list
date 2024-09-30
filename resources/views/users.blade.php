1e03c749deb0517fdaaa060e8a1529d9c9c6a101
Messmer <prokopovaleksandr544@gmail.com>
2024-09-30 17:36:48 +0700

    add blade extend and section

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,3 +1,4 @@
-<div>
-    <!-- People find pleasure in different ways. I find it in keeping my mind clear. - Marcus Aurelius -->
-</div>
+@extends('layouts.layout')
+@section('content')
+
+@endsection
