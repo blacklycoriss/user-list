aaaa56d6edd9806a8410fe155c103cb31634725d
Messmer <prokopovaleksandr544@gmail.com>
2024-09-30 17:54:23 +0700

    add users output

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,4 +1,9 @@
 @extends('layouts.layout')
 @section('content')
+    <h3>Все пользователи сайта</h3>
+    <p>Имя | Электронная почта</p>
+    @foreach($users as $user)
+        <li>{{ $user->name . ' | ' . $user->email}}</li>
+    @endforeach
 
 @endsection
